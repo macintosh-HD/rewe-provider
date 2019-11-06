d4f5eb8e95b072575041022dc46131a0a368dfb4
Julian Gentges <JulGentges@gmail.com>
2019-11-06 11:03:18 +0100

    changed type of 'ReweGeoLocation.latitude' & '.longitude' from Double to String

--- Sources/ReweProvider/Models/AdditionalTypes.swift
+++ Sources/ReweProvider/Models/AdditionalTypes.swift
@@ -1,8 +1,8 @@
 import Vapor
 
 public struct ReweGeoLocation: Content {
-    public let latitude: Double
-    public let longitude: Double
+    public let latitude: String
+    public let longitude: String
     public let city: String
     public let state: String
     public let success: Bool
